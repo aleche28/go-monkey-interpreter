8aca0e7c58df6a7d36a4f8592caf048146b6af3a
Alessio Chessa <ale.chessa28@gmail.com>
2026-09-05 10:46:33 +0200

    feat: add evaluate step in repl

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
 
-const PROMPT = ">> "
-const MONKEY_FACE = `            __,__
+const (
+	PROMPT      = ">> "
+	MONKEY_FACE = `            __,__
    .--.  .-"     "-.  .--.
   / .. \/  .-. .-.  \/ .. \
  | |  '|  /   Y   \  |'  | |
@@ -21,6 +23,7 @@ const MONKEY_FACE = `            __,__
         '._ '-=-' _.'
            '-----'
 `
+)
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -37,14 +40,16 @@ func Start(in io.Reader, out io.Writer) {
 		p := parser.New(l)
 
 		program := p.ParseProgram()
-
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
