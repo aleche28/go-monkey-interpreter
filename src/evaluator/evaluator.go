d2cd9831734adf6966aa06dd87485ac8705762a8
Alessio Chessa <ale.chessa28@gmail.com>
2026-09-08 23:02:17 +0200

    feat: add builtin function 'len'

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -241,12 +241,15 @@ func evalBlockStatement(stmt *ast.BlockStatement, env *object.Environment) objec
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: %s", node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+
+	return newError("identifier not found: %s", node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
@@ -275,22 +278,27 @@ func isError(obj object.Object) bool {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+
+	case *object.Function:
+		// extend function env
+		env := object.NewEnclosedEnvironment(fn.Env)
+
+		// bind args in env
+		for i, param := range fn.Parameters {
+			env.Set(param.Value, args[i])
+		}
+
+		// need to unwrap, otherwise a return statement would bubble up
+		// through several functions and stop the evaluation in all of them
+		return unwrapReturnValue(Eval(fn.Body, env))
+
+	case *object.Builtin:
+		return fn.Fn(args...)
+
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-
-	// extend function env
-	env := object.NewEnclosedEnvironment(function.Env)
-
-	// bind args in env
-	for i, param := range function.Parameters {
-		env.Set(param.Value, args[i])
-	}
-
-	// need to unwrap, otherwise a return statement would bubble up
-	// through several functions and stop the evaluation in all of them
-	return unwrapReturnValue(Eval(function.Body, env))
 }
 
 func unwrapReturnValue(obj object.Object) object.Object {
