f4de3cac61b547735cc3054d6e634d2920b0c3c3
Alessio Chessa <ale.chessa28@gmail.com>
2026-09-10 21:36:32 +0200

    feat: add puts builtin

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -94,6 +97,16 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `push` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
